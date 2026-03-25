dcf3df25729061811287062b89b62a710ba88dae
hinm163 <zyq636120@163.com>
2026-03-25 23:49:41 +0800

    Add a blank sepolicy template

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,5 +29,12 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # Recovery
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
+# Sepolicy
+include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
+
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/pagani/BoardConfigVendor.mk
