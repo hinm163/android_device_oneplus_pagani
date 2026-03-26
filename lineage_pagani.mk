3af6e539b667429a0ced9135f8181f4bf0f01dc8
hinm163 <zyq636120@163.com>
2026-03-26 14:10:37 +0800

    adjust name of Oneplus 13s

--- lineage_pagani.mk
+++ lineage_pagani.mk
@@ -18,14 +18,14 @@ PRODUCT_NAME := lineage_pagani
 PRODUCT_DEVICE := pagani
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := PKX110
+PRODUCT_MODEL := CPH2723
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="qssi_64-user 16 BP2A.250605.015 1764756120256 release-keys" \
-    BuildFingerprint=OnePlus/PKX110/OP60F5L1:16/AP3A.240617.008/V.1d6b086_3e842d_3e842a:user/release-keys \
-    DeviceName=OP60F5L1 \
-    DeviceProduct=PKX110 \
-    SystemDevice=OP60F5L1 \
-    SystemName=PKX110
+    BuildFingerprint=OnePlus/CPH2723/OP612BL1:16/AP3A.240617.008/V.1d6b086_3e842d_3e842a:user/release-keys \
+    DeviceName=OP612BL1 \
+    DeviceProduct=CPH2723 \
+    SystemDevice=OP612BL1 \
+    SystemName=CPH2723
